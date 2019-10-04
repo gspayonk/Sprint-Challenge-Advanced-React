4843afcabc2e35e17ea4d5444218c33105aa75c3
gspayonk <grissobel.castro@gmail.com>
2019-10-04 13:59:00 -0400

    MVP + stretch

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//importing testing library
+import { render } from "@testing-library/react";
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('has header', () => {
+  const doc = render(<App />);
+  doc.queryByText(/players/i)
+})
+
+// it('has dark-mode', () => {
+//   const doc = render(<App />);
+//   doc.queryByText(/players/i)
+// })
